c400470c5e5733199c4e637c581b29c316cffdfa
[Ezekiel Narvasa ] <114404058+Nymoooo@users.noreply.github.com>
2023-07-27 15:53:19 +0800

    .

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -14,7 +14,7 @@ const Banner = () => {
       </div>
       {/* Description */}
       <div className="w-3/4 flex flex-col items-center md:items-start    gap-2">
-      <h1 className="text-2xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
+      <h1 className="text-2xl md:text-5xl font-bold text-white">zekiel Narvasa</h1>
         <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
           Full Stack Developer
         </h3>
