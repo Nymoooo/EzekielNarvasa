80beefc0e9901aa4dc1a7751b59604d4325785f2
[Ezekiel Narvasa ] <114404058+Nymoooo@users.noreply.github.com>
2023-08-01 18:19:34 +0800

    .

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,15 +15,12 @@ const Banner = () => {
         </div>
         {/* Description */}
         <div className="w-3/4 flex flex-col items-center md:items-start gap-2">
-      <h1 className="text-2xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
-        <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-          Full Stack Developer
-        </h3>
+          <h1 className="text-2xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
+          <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
+            Full Stack Developer
+          </h3>
           <p className="text-sm md:text-base lg:text-lg tracking-wide text-center md:text-left">
-            An incoming fourth-year computer science student looking for an IT internship who
-            possesses a solid fundamental foundation in programming, data structures, and
-            algorithms, with coursework and independent studies in computer networking, information
-            security, and cryptography.
+            An incoming fourth-year computer science student who possesses a solid fundamental foundation in programming, data structures, and algorithms, with computer networking, independent studies in information security and cryptography.
           </p>
         </div>
       </div>
