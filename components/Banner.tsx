200aa0f1d677d8a373af2516e001ae580ad7fa28
[Ezekiel Narvasa ] <114404058+Nymoooo@users.noreply.github.com>
2023-08-01 18:14:18 +0800

    .

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import ProfileImg from "../public/img/profileImg.png";
 
 const Banner = () => {
   return (
-    <div className="container mx-auto px-4 md:px-8 py-12">
+    <div className="wrapper container mx-auto px-4 md:px-8 py-12">
       <div className="flex flex-col md:flex-row items-center md:items-stretch gap-10">
         {/* Image */}
         <div className="w-[11rem] h-[11rem] md:w-[13rem] md:h-[13rem] rounded-full bg-black">
@@ -14,11 +14,11 @@ const Banner = () => {
           />
         </div>
         {/* Description */}
-        <div className="w-full md:w-3/4 flex flex-col items-center md:items-start gap-4 md:gap-6">
-          <h1 className="text-xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
-          <h2 className="text-lg md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-            Full Stack Developer
-          </h2>
+        <div className="w-3/4 flex flex-col items-center md:items-start gap-2">
+      <h1 className="text-2xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
+        <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
+          Full Stack Developer
+        </h3>
           <p className="text-sm md:text-base lg:text-lg tracking-wide text-center md:text-left">
             An incoming fourth-year computer science student looking for an IT internship who
             possesses a solid fundamental foundation in programming, data structures, and
