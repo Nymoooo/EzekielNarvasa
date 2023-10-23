65142f4e4ac22bc65c01041a1f69974c12f30223
Ezekiel Narvasa <narvasa.ezekiel.bscs2020@gmail.com>
2023-10-23 20:58:32 +0800

    .

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -8,13 +8,10 @@ function Aboutme() {
         <Title text="About Me" icon={<SiInformatica />} />
         <div className="text-lg tracking-wide flex flex-col gap-6 ">
           <p>
-            I am a graduating Bachelor of Science in Computer Science student at
-            the University of Caloocan City, Philippines, with solid grasp in
-            programming, website development, and website content writing. My CS
-            journey involves optimizing AI tools and techs like NPM, Vercel, and
-            Git. Embracing new tech fuels my passion, while leading teams using
-            different project development approaches sharpens my confidence and
-            communication strengths.
+            My Computer Science journey has led me to optimize AI tools and
+            familiarize technologies like NPM, Vite, Xampp, Vercel, and Git.
+            Embracing emerging tech fuels my passion, while leading project
+            teams hones my communication and leadership skills.
           </p>
           <p>
             I am always looking to improve my skills and stay up-to-date with
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -14,12 +14,16 @@ const Banner = () => {
       </div>
       {/* Description */}
       <div className="w-3/4 flex flex-col items-center md:items-start    gap-2">
-      <h1 className="text-2xl md:text-5xl font-bold text-white">Ezekiel Narvasa</h1>
+        <h1 className="text-2xl md:text-5xl font-bold text-white">
+          Ezekiel Narvasa
+        </h1>
         <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-          Full Stack Developer
+          Aspiring Full-Stack Web Developer
         </h3>
         <p className="text-base tracking-wide text-center md:text-left">
-          An incoming fourth-year computer science student looking for an IT internship who possesses a solid fundamental foundation in programming, data structures, and algorithms, with coursework and independent studies in computer networking, information security, and cryptography.
+          I&apos;m a soon-to-be graduate in Computer Science at the University
+          of Caloocan City, Philippines, with a strong foundation in
+          programming, web development, and website content creation.
         </p>
       </div>
     </div>
